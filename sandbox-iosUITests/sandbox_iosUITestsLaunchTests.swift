6d0a327cb2e04b51baf9e2e71bfddc42f8480e78
smapira <smapira@routeflags.com>
2022-09-27 23:39:26 +0900

    Initial Commit

---
+++ sandbox-iosUITests/sandbox_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sandbox_iosUITestsLaunchTests.swift
+//  sandbox-iosUITests
+//
+//  Created by bookair18 on 2022/09/27.
+//
+
+import XCTest
+
+class sandbox_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
